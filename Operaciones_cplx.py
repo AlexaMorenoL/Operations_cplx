f68fb394f2d900885572c3e252188a34d37c8c57
AlexaMorenoL <142949623+AlexaMorenoL@users.noreply.github.com>
2023-09-07 09:48:08 -0500

    Operaciones_cplx.py

--- Operaciones_cplx.py
+++ Operaciones_cplx.py
@@ -1,48 +1,60 @@
 import numpy as np
 import math
+
+
 def sumar_vectores(vector1, vector2):
     """Calcula la suma de dos vectores complejos."""
     suma = np.add(vector1, vector2)
     return suma
+
 
 def inverso_vector(vector):
     """ Calcula el inverso aditivo de un vector complejo."""
     inverso = np.negative(vector)
     return inverso
 
+
 def mult_escalar_vector(escalar, vector):
     """Calcula la multiplicación de un escalar por un vector complejo."""
-    multiplicacion =np.multiply(escalar, vector)
+    multiplicacion = np.multiply(escalar, vector)
     return multiplicacion
+
 
 def sumar_matrices_complejas(matriz_1, matriz_2):
     """ Realiza la suma de dos matrices complejas."""
     suma_matrices = np.add(matriz_1, matriz_2)
     return suma_matrices
 
+
 def inversa_matriz(matriz):
     """ Calcula la inversa aditiva de una matriz compleja."""
     inversa_mat = np.negative(matriz)
     return inversa_mat
+
 
 def mult_escalar_matriz(escalar, matriz):
     """Realiza la multiplicación de un escalar por una matriz compleja."""
     mult_escalar_mat = np.multiply(escalar, matriz)
     return mult_escalar_mat
 
+
 def matriz_transpuesta(matriz):
     """ Calcula la transpuesta de una matriz o vector complejo."""
     transpuesta = np.transpose(matriz)
     return transpuesta
+
+
 def matriz_conjugada(matriz):
     """ Calcula la matriz conjugada de una matriz o vector complejo."""
     conjugada = np.conjugate(matriz)
     return conjugada
 
+
 def adjunta(matriz):
     """Calcula la adjunta (daga) de una matriz o vector"""
     adjunta = matriz_transpuesta(matriz_conjugada(matriz))
     return adjunta
+
 
 def producto_matrices(mat1, mat2):
     """Calcula el producto entre 2 matrices (de tamaños compatibles)"""
@@ -60,6 +72,7 @@ def producto_matrices(mat1, mat2):
                     matriz[i][j] += mat1[i][k] * mat2[k][j]
         return matriz
 
+
 def accion(matriz, vector):
     """Calcula la "acción" de una matriz sobre un vector."""
     matriz = np.array(matriz)
@@ -67,20 +80,23 @@ def accion(matriz, vector):
     rest = np.dot(matriz, vector)
     return str(rest)
 
-def producto_interno(vector_1,vector_2):
+
+def producto_interno(vector_1, vector_2):
     """Calcula el Producto interno de dos vectores"""
     suma = 0
     for i in range(len(vector_1)):
-        suma += vector_1[i]*vector_2[i]
+        suma += vector_1[i] * vector_2[i]
     return suma
+
 
 def norma_vector(vector):
     """Calcula la norma de un vector."""
     suma = 0
     for i in range(len(vector)):
-        suma += abs(vector[i])**2
+        suma += abs(vector[i]) ** 2
         print(suma)
-    return round((suma)**0.5, 2)
+    return round((suma) ** 0.5, 2)
+
 
 def distancia_vectores(vector1, vector2):
     """Calcula la distancia entre dos vectores."""
@@ -91,14 +107,30 @@ def distancia_vectores(vector1, vector2):
     distancia = math.sqrt(suma_cuadrados)
     return distancia
 
+
 def valor_vector_propio(matriz):
     """Calcula los valores  y vectores propios de una matriz."""
     valores_propios = np.linalg.eig(matriz)
     vectores_propios = np.linalg.eig(matriz)
     return valores_propios, vectores_propios
 
+
+def producto_tensor_matriz(A, B):
+    m = len(A)
+    n = len(B)
+    m1 = len(A[0])
+    n1 = len(B[0])
+    fila = m * n
+    columna  = m1 * n1
+    resultado = [[0 for fil in range (fila)] for colum in range (columna)]
+    for j in range (fila):
+        for k in range (columna):
+            resultado[j][k] = (A[j//n][k//n1]*B[j%n][k%n1])
+    return resultado
+
+
 if __name__ == '__main__':
-    """Estos print se utilizan para probar el correcto funcionamiento de cada función 
+    """Estos print se utilizan para probar el correcto funcionamiento de cada función
     (Herramienta del programador)"""
     # Casos de prueba para las funciones de operaciones con números complejos
     """Para hacer correcto uso de esta librería se debe tener en cuenta que 
@@ -112,6 +144,8 @@ if __name__ == '__main__':
     mat2 = [[2 - 1j, 4 + 3j], [1 - 2j, 5 + 4j]]
     mat3 = [[1 + 2j, 3 - 1j], [0 + 1j, -2 - 3j]]
 
+    A = [[1, 2], [2, 1]]
+    B = [[4, 5,], [5, 4]]
     # Pruebas para las funciones de operaciones con números complejos
     print("Suma de vectores complejos:", sumar_vectores(vec1, vec2))
     print("Inverso de vector complejo:", inverso_vector(vec1))
@@ -123,4 +157,11 @@ if __name__ == '__main__':
 
     print("Matriz transpuesta:\n", matriz_transpuesta(mat3))
     print("Matriz conjugada:\n", matriz_conjugada(mat3))
-    
+
+    print("----------------------------------Prueba ult def------------------------------")
+    print(A)
+    print(B)
+    print("El prodcuto tensor de 2 matrices es:",  producto_tensor_matriz(A, B))
+
+
+    print("--------------------Prueba ")
